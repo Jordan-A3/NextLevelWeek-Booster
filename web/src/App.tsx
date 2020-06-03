59afdace18101a4aed2910c5ff56ee408eb91f08
Ananda Reis <ananda.queiroz50@gmail.com>
2020-06-03 17:02:09 -0300

    logo frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import lampadas from './baterias.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={lampadas} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World!</h1>
       </header>
     </div>
